63a4d2ea64f114f25ac5d78c7b4ed32fb612c0d3
Romain Dabadie <romain.dabadie@riotinto.com>
2024-08-29 03:30:47 -0400

    MyFirstPackage

---
+++ ch15/MyFirstPackage/MyFirstPackage/lib/TemperatureConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyFirstPackage.lib
+{
+    public static class TemperatureConverter
+    {
+        public static double ConvertCelsiusToFahrenheit(double celsius)
+        {
+            return celsius * 9 / 5 + 32;
+        }
+
+        public static double ConvertFahrenheitToCelsius(double fahrenheit)
+        {
+            return (fahrenheit - 32) * 5 / 9;
+        }
+    }
+}
