f609318488156df797cfa4ba143cae2e303373f1
Romain Dabadie <romain.dabadie@riotinto.com>
2024-09-24 11:37:44 -0400

    MAUIdemo

---
+++ ch05/MauiDemo/MauiDemo/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace MauiDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnButtonClicked(object sender, EventArgs e)
+        {
+            MessageLabel.Text = "You clicked the button!";
+        }
+    }
+}
