71abc88eb17eb99f186063ba47f56dfba4bec31a
Romain Dabadie <romain.dabadie@riotinto.com>
2024-09-24 11:28:47 -0400

    MemoryUsage

---
+++ ch04/MemoryUsage/MemoryUsage/Program.cs
@@ -0,0 +1,18 @@
+﻿List<string> list = new();
+
+while (true)
+{
+    list.Add(GenRandomStr(10000));
+    Thread.Sleep(1);
+}
+static string GenRandomStr(int length)
+{
+    Random rnd = new();
+    var chars = new List<char>();
+    for (var i = 0; i < length; i++)
+    {
+        var a = (char)rnd.Next(65, 122);
+        chars.Add(a);
+    }
+    return string.Concat(chars);
+}
